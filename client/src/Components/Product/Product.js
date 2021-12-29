834ea0ac72f6518ecf6a0f79146560819c982fd6
arianna choza <unachoza@gmail.com>
2021-12-28 23:46:01 -0500

    contact pages plus nonsense

---
+++ client/src/Components/Product/Product.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Product.css';
+
+const Product = () => {
+  return (
+    <div className="product">
+      This is a product for sale
+    </div>
+  );
+}
+
+export default Product;
---
+++ client/src/Components/ProductList/ProductList.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Product from './Product/Product'
+import './ProductList.css';
+
+const ProductList = () => {
+  return (
+    <div className="App">
+      <Product/>
+    </div>
+  );
+}
+
+export default ProductList;
